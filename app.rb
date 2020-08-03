82832804f29c782f0183b04e705dbc15059b5437
Joseph Kao <josephtkao@gmail.com>
2020-08-03 21:32:53 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ class App < Sinatra::Base
   get "/say/:number/:phrase" do
     @number= params[:number].to_i
     @phrase = params[:phrase]
-    times.@number do
+    @number.times do
       "#{@phrase}"
     end
   end
